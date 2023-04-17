ef38fa1f1638130d71425c4bdadaf14bc00367dd
Igor Kozhevnikov <igor.kozhevnikov@proton.me>
2023-04-17 22:04:05 +0300

    Refactoring the Application class

--- src/Application/Application.php
+++ src/Application/Application.php
@@ -6,8 +6,7 @@ namespace Cross\Application;
 
 use Cross\Config\Config;
 use Cross\Plugin\PluginInterface;
-use Cross\Plugin\Plugins;
-use Symfony\Component\Console\Application as BaseApplication;
+use Symfony\Component\Console\Application as CoreApplication;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -20,14 +19,14 @@ class Application
     /**
      * Base application.
      */
-    private BaseApplication $application;
+    private CoreApplication $application;
 
     /**
      * Constructor.
      */
     public function __construct(string $name, string $version)
     {
-        $this->application = new BaseApplication($name, $version);
+        $this->application = new CoreApplication($name, $version);
     }
 
     /**
@@ -41,21 +40,29 @@ class Application
     }
 
     /**
-     * Treats a plugins list.
+     * Sets plugins.
      *
      * @param array<array-key, class-string|PluginInterface> $plugins
      */
     public function plugins(array $plugins): void
     {
-        $initializer = fn (string|PluginInterface $plugin): PluginInterface => is_string($plugin) ? new $plugin() : $plugin;
-
-        /** @var PluginInterface[] $plugins */
-        $plugins = array_map($initializer, $plugins);
-
         foreach ($plugins as $plugin) {
-            Config::set($plugin->getKey(), $plugin->getConfig());
-            $this->set($plugin->getCommands());
+            $this->plugin($plugin);
         }
+    }
+
+    /**
+     * Sets a plugin.
+     */
+    public function plugin(string|PluginInterface $plugin): void
+    {
+        if (is_string($plugin)) {
+            $plugin = new $plugin();
+        }
+
+        Config::set($plugin->getKey(), $plugin->getConfig());
+
+        $this->commands($plugin->getCommands());
     }
 
     /**
@@ -63,17 +70,17 @@ class Application
      *
      * @param array<class-string|Command> $commands
      */
-    public function set(array $commands): void
+    public function commands(array $commands): void
     {
         foreach ($commands as $command) {
-            $this->add($command);
+            $this->command($command);
         }
     }
 
     /**
      * Adds a command.
      */
-    public function add(string|Command $command): void
+    public function command(string|Command $command): void
     {
         if (is_string($command)) {
             $command = new $command();
